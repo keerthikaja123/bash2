4de232e60663d9f71591dee0629ad9199f385d83
keerthikaja123 <keerthikaja09@gmail.com>
2023-01-09 17:16:37 +0530

    klm file

---
+++ klm2.java
@@ -0,0 +1,13 @@
+package Bash2;
+
+public class klm2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("klm");
+
+  System.out.println("klm");
+
+	}
+
+}
